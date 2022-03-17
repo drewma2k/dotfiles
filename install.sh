5bce5c244a629d9ed44e390fac956a867b432997
Drew Abram <drew.abram@capitalone.com>
2022-03-17 11:38:25 -0400

    add tmux config

--- install.sh
+++ install.sh
@@ -2,10 +2,14 @@
 
 DIR=${PWD}
 
+# vim
 mkdir $HOME/.vim
 ln -is $DIR/vim/.vimrc $HOME/.vim/.vimrc
 
+# zsh
 ln -is $DIR/zsh/.zshrc $HOME/.zshrc
 mkdir $HOME/.oh-my-zsh/custom
 ln -is $DIR/zsh/oh-my-zsh/aliases.zsh $HOME/.oh-my-zsh/custom/aliases.zsh 
 
+# tmux
+ln -is $DIR/tmux.conf $HOME/.tmux.conf
