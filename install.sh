ae28a4cf031eddb94f2fc79e4d375e9174b3208b
Drew Abram <drew.abram@gmail.com>
2022-04-07 20:07:18 -0700

    add tmux config

--- install.sh
+++ install.sh
@@ -9,3 +9,6 @@ ln -is $DIR/zsh/.zshrc $HOME/.zshrc
 mkdir $HOME/.oh-my-zsh/custom
 ln -is $DIR/zsh/oh-my-zsh/aliases.zsh $HOME/.oh-my-zsh/custom/aliases.zsh 
 
+# tmux
+ln -is $DIR/tmux.conf $HOME/.tmux.conf
+
