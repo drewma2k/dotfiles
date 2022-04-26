1af027a7bf365c7f913d55a029a00fd482064d54
Drew Abram <drew.abram@gmail.com>
2022-04-25 19:02:38 -0700

    merge in work changes

--- install.sh
+++ install.sh
@@ -2,15 +2,22 @@
 
 DIR=${PWD}
 
+# vim
 mkdir $HOME/.vim
 ln -is $DIR/vim/vimrc $HOME/.vim/vimrc
 
+# zsh
 ln -is $DIR/zsh/.zshrc $HOME/.zshrc
 mkdir $HOME/.oh-my-zsh/custom
 ln -is $DIR/zsh/oh-my-zsh/aliases.zsh $HOME/.oh-my-zsh/custom/aliases.zsh 
 
 # tmux
 ln -is $DIR/tmux.conf $HOME/.tmux.conf
+
+# scripts
+mkdir $HOME/bin
+ln -is $DIR/scripts/ide.sh $HOME/bin/ide
+chmod +x $HOME/bin/ide
 
 # nvim
 ln -is $DIR/nvim/init.vim $HOME/.config/nvim/init.vim
