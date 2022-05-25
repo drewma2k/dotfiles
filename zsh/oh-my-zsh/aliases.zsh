9267102db3a13a1a5b362e9a44bf54fac0f8364f
Drew Abram <drew.abram@gmail.com>
2022-05-25 11:10:39 -0700

    update aliases

--- zsh/oh-my-zsh/aliases.zsh
+++ zsh/oh-my-zsh/aliases.zsh
@@ -1,10 +1,12 @@
 # Custom aliases
 
 # editing config files
-alias aliases='vim ~/.oh-my-zsh/custom/aliases.zsh'
-alias zshrc='vim ~/.zshrc'
+alias aliases='nvim ~/.oh-my-zsh/custom/aliases.zsh'
+alias zshrc='nvim ~/.zshrc'
 alias vimrc='vim ~/.vim/vimrc'
+alias nvimrc='nvim ~/.config/nvim/init.lua'
 alias dotfiles='cd ~/src/dotfiles'
+alias src='cd ~/src'
 
 # formatted date that will sort properly
 alias datef='date +%Y-%m-%d'
