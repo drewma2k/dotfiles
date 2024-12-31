acf1a8c8e28f17259b2bd43eb0a33bcd390275e4
Drew Abram <drew.abram@gmail.com>
2024-12-31 10:53:42 -0500

    fish config

---
+++ configs/config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+	fish_hybrid_key_bindings
+	set PATH $PATH /opt/homebrew/bin
+	set SHELL /opt/homebrew/bin/fish	
+	set fish_greeting
+end
